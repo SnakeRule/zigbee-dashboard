3456c471a53bc3e4ec4bb056da58d43b224f0d23
Jere-Joonas Valtanen <jerevaltanen@gmail.com>
2026-04-28 11:55:23 +0300

    Production build doesn't use turbopack root

--- dashboard/next.config.ts
+++ dashboard/next.config.ts
@@ -1,16 +1,14 @@
 import type { NextConfig } from "next";
+import { PHASE_DEVELOPMENT_SERVER } from "next/constants";
 
-const nextConfig: NextConfig = {
-  output: "standalone",
-  images: {
-    remotePatterns: [
-      new URL("https://openweathermap.org/payload/api/media/file/*.png/"),
-    ],
-  },
-  turbopack: {
-    root: "/",
-  },
-  /* config options here */
-};
-
-export default nextConfig;
+export default function nextConfig(phase: string): NextConfig {
+  return {
+    output: "standalone",
+    images: {
+      remotePatterns: [
+        new URL("https://openweathermap.org/payload/api/media/file/*.png/"),
+      ],
+    },
+    ...(phase === PHASE_DEVELOPMENT_SERVER ? { turbopack: { root: "/" } } : {}),
+  };
+}
